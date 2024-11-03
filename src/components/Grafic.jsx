05148aaaac4bc08d46d3ca1d26f23bb472347245
ronesanttos <devronesantos@gmail>
2024-11-03 19:25:15 -0300

    Tentar depois

--- src/components/Grafic.jsx
+++ src/components/Grafic.jsx
@@ -3,39 +3,58 @@ import {
   Bar,
   XAxis,
   Tooltip,
-  Legend,
   ResponsiveContainer,
+  Cell,
+  LabelList,
 } from "recharts";
 import data from "../data.json";
-
+import { useState } from "react";
+//Styled Components: Ótimo para projetos que usam CSS-in-JS e desejam um estilo modular.
 const Grafic = () => {
-  const CustomTooltip = ({ active }) => {
-    if (active && data && data.length) {
-        return (
-            <div style={{ backgroundColor: '#fff', border: '1px solid #ccc', padding: '5px' }}>
-                <p>{`Mês: ${data[0].day}`}</p>
-                <p>{`Despesa: $${data[0].amount}`}</p>
-            </div>
-        );
-    }
-    return null; //mudar depois
-};
+  const [datas, setData] = useState(data);
+  const [selectedValue, setSelectedValue] = useState(null);
 
+  const CustomLabel = ({ x, y }) => (
+    <text
+      className="custom-label"
+      x={x}
+      y={y}
+      fill="Black"
+      textAnchor="start"
+      dominantBaseline="start"
+      fontWeight="bold"
+    >
+      {selectedValue?.amount}
+    </text>
+  );
+
+  const handleClick = (entry) => {
+    const newData = datas.find((dados) => dados.amount == entry.amount);
+    setSelectedValue(newData);
+  };
   return (
-    <ResponsiveContainer width="100%" height={300} >
-      <BarChart data={data} >
-        <XAxis dataKey="day" tick={{fill: 'hsl(28, 10%, 53%)'}}/>
-        <Tooltip content={<CustomTooltip/>} />
-        <Legend />
-        <Bar 
-          type="monotone"
-          dataKey="amount"
-          activeDot={{ r: 8 }}
-          radius={5}
-          style={{fill:'hsl(10, 79%, 65%)'}}
-        />
-      </BarChart>
-    </ResponsiveContainer>
+    <div>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={datas}>
+          <XAxis dataKey="day" tick={{ fill: "hsl(28, 10%, 53%)" }} />
+          <Bar dataKey="amount" radius={5}>
+            {datas.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                onClick={() => handleClick(entry)}
+                cursor="pointer"
+                fill={
+                  entry.day === "wed"
+                    ? " hsl(186, 34%, 60%)"
+                    : "hsl(10, 79%, 65%)"
+                }
+              />
+            ))}
+            <LabelList dataKey="amount" content={<CustomLabel />} />
+          </Bar>
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
   );
 };
 
